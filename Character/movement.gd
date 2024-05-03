7fa985666db8d28e883f416192b9533c21df8605
Kevin Yook <yook00627@gmail.com>
2024-05-02 22:40:50 -0700

    Added title and world 1 scene with pipeman

--- Character/movement.gd
+++ Character/movement.gd
@@ -6,7 +6,7 @@ const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+@onready var _animated_sprite = $AnimatedSprite2D
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -14,15 +14,22 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
+	var direction = Input.get_axis("Left", "Right")
+	if direction < 0:
 		velocity.x = direction * SPEED
+		_animated_sprite.flip_h = true
+		_animated_sprite.play("movement")                                 
+	elif direction > 0:
+		velocity.x = direction * SPEED
+		_animated_sprite.flip_h = false
+		_animated_sprite.play("movement")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		_animated_sprite.play("idle")
 
-	#move_and_slide()
+	move_and_slide()
