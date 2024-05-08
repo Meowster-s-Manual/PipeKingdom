a10958e9d985b8a8ece11f063345ef00a00c2484
Kevin Yook <yook00627@gmail.com>
2024-05-07 19:41:21 -0700

    added UI basics

--- World/Camera2D.gd
+++ World/Camera2D.gd
@@ -1,10 +1,9 @@
 extends Camera2D
-const CAMERA_OFFSET = 576
+const CAMERA_OFFSET = 1280/2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -12,8 +11,6 @@ func _process(delta):
 		set_limit(SIDE_LEFT,get_screen_center_position()[0] - CAMERA_OFFSET)
 	else:
 		set_limit(SIDE_LEFT,0)
-	set_limit(SIDE_BOTTOM, 647)
 	limit_smoothed = true
 	var viewport := get_viewport()
 	var camera := viewport.get_camera_2d()
-	print(get_screen_center_position()[0])
