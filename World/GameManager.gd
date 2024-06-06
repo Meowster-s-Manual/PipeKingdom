84ae1d91ab7aeface46bf75b4ef86a188fc6b7fd
Kevin Yook <yook00627@gmail.com>
2024-06-05 23:26:23 -0700

    cleanup and annotation

--- World/GameManager.gd
+++ World/GameManager.gd
@@ -5,7 +5,7 @@ var BrickBreak = preload("res://Particles/BrickBreak.tscn")
 
 func break_block(CollisionObject):
 	if _Bricks.get_children().has(CollisionObject) == true:
-		var brickBreak = BrickBreak.instantiate()
+		var brickBreak: Node2D = BrickBreak.instantiate()
 		brickBreak.position = CollisionObject.get_position()
 		_Bricks.add_child(brickBreak)
 		CollisionObject.queue_free()
