75c794bd7c217bb66514b384bdc2dda00676fb98
andrew-suh <andrew.suh.94@gmail.com>
2024-06-20 20:47:06 -0700

    Added fireball

---
+++ PreFab/fireball.gd
@@ -0,0 +1,15 @@
+extends RigidBody2D
+var velocity = Vector2(150,175)
+var timer = 200
+
+func _physics_process(delta):
+	var collision = move_and_collide(velocity * delta)
+	if collision:
+		if collision.get_angle() != 0:
+			queue_free()
+		velocity = velocity.bounce(collision.get_normal())
+
+	# destry self after time pass
+	timer -= 1
+	if (timer == 0):
+		queue_free()
